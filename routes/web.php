21224114943254a20c7bfc6c2e44666258458cd8
ahmdsyauqi <ahmadsyauqi.stmik2014@gmail.com>
2019-02-12 22:25:56 +0800

    re-routing

--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,18 @@
 <?php
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('auth.login');
 });
 
 Auth::routes();
 
-Route::get('/card', 'HomeController@index')->name('card');
+Route::get('/home', 'HomeController@index')->name('template.dashboard');
+
 // dashboard
 Route::view('/template/dashboard', 'template.dashboard');
+// auth pages
+Route::view('/template/login', 'template.authLogin');
+Route::view('/template/register', 'template.authRegister');
+Route::view('/template/forget', 'template.authForgetPassword');
 // component
 Route::view('/template/button', 'template.button');
 Route::view('/template/card', 'template.card');

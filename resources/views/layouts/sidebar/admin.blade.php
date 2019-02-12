87a9cea2a1d6258512f2a431dd0fa471812095ec
ahmdsyauqi <ahmadsyauqi.stmik2014@gmail.com>
2019-02-12 22:25:42 +0800

    sidebar menu sample

--- resources/views/layouts/sidebar/admin.blade.php
+++ resources/views/layouts/sidebar/admin.blade.php
@@ -1,58 +1,42 @@
-<!-- Nav Item - Dashboard -->
 <li class="nav-item">
   <a class="nav-link" href="/home">
     <i class="fas fa-fw fa-tachometer-alt"></i>
     <span>Dashboard</span></a>
 </li>
-
 <!-- Divider -->
 <hr class="sidebar-divider">
-
 <!-- Heading -->
 <div class="sidebar-heading">
-  Interface
+  Sample Header or Grup Menu
 </div>
-
+{{-- nav item singgle menu without collapse --}}
+<li class="nav-item">
+  <a class="nav-link" href="#">
+    <i class="fas fa-fw fa-list"></i>
+    <span>Sample Menu 1</span></a>
+</li>
 <!-- Nav Item - Pages Collapse Menu -->
 <li class="nav-item">
-  <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true"
-    aria-controls="collapseTwo">
+  <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#sampleMenu" aria-expanded="true"
+    aria-controls="sampleMenu">
     <i class="fas fa-fw fa-cog"></i>
-    <span>Components</span>
+    <span>Sample Menu 2</span>
   </a>
-  <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+  <div id="sampleMenu" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
     <div class="bg-white py-2 collapse-inner rounded">
-      <h6 class="collapse-header">Custom Components:</h6>
-      <a class="collapse-item" href="buttons.html">Buttons</a>
-      <a class="collapse-item" href="cards.html">Cards</a>
+      <h6 class="collapse-header">Sample Header :</h6>
+      <a class="collapse-item" href="#">Menu 2.1</a>
+      <a class="collapse-item" href="#">Menu 2.2</a>
+      <a class="collapse-item" href="#">Menu 2.2</a>
     </div>
   </div>
 </li>
-
-<!-- Nav Item - Utilities Collapse Menu -->
-<li class="nav-item">
-  <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true"
-    aria-controls="collapseUtilities">
-    <i class="fas fa-fw fa-wrench"></i>
-    <span>Utilities</span>
-  </a>
-  <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
-    <div class="bg-white py-2 collapse-inner rounded">
-      <h6 class="collapse-header">Custom Utilities:</h6>
-      <a class="collapse-item" href="utilities-color.html">Colors</a>
-      <a class="collapse-item" href="utilities-border.html">Borders</a>
-      <a class="collapse-item" href="utilities-animation.html">Animations</a>
-      <a class="collapse-item" href="utilities-other.html">Other</a>
-    </div>
-  </div>
-</li>
-
 <!-- Divider -->
 <hr class="sidebar-divider">
 
 <!-- Heading -->
 <div class="sidebar-heading">
-  Addons
+  Devlopement
 </div>
 
 <!-- Nav Item - Pages Collapse Menu -->
@@ -63,28 +47,27 @@
   </a>
   <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
     <div class="bg-white py-2 collapse-inner rounded">
-      <h6 class="collapse-header">Login Screens:</h6>
-      <a class="collapse-item" href="login.html">Login</a>
-      <a class="collapse-item" href="register.html">Register</a>
-      <a class="collapse-item" href="forgot-password.html">Forgot Password</a>
+      <h6 class="collapse-header">Login Pages:</h6>
+      <a class="collapse-item" href="/template/login">Login</a>
+      <a class="collapse-item" href="/template/register">Register</a>
+      <a class="collapse-item" href="/template/forget">Forgot Password</a>
       <div class="collapse-divider"></div>
-      <h6 class="collapse-header">Other Pages:</h6>
-      <a class="collapse-item" href="404.html">404 Page</a>
-      <a class="collapse-item" href="blank.html">Blank Page</a>
+      <h6 class="collapse-header">Component :</h6>
+      <a class="collapse-item" href="/template/button">Button</a>
+      <a class="collapse-item" href="/template/card">Card</a>
+      <div class="collapse-divider"></div>
+      <h6 class="collapse-header">Utility :</h6>
+      <a class="collapse-item" href="/template/color">Color</a>
+      <a class="collapse-item" href="/template/border">Border</a>
+      <a class="collapse-item" href="/template/animation">Animation</a>
+      <a class="collapse-item" href="/template/other">Other</a>
+      <div class="collapse-divider"></div>
+      <h6 class="collapse-header">Other Pages :</h6>
+      <a class="collapse-item" href="/template/dashboard">Dashboard</a>
+      <a class="collapse-item" href="/template/blank">Blank Page</a>
+      <a class="collapse-item" href="/template/data">Table</a>
+      <a class="collapse-item" href="/template/chart">Chart</a>
+      <div class="collapse-divider"></div>
     </div>
   </div>
-</li>
-
-<!-- Nav Item - Charts -->
-<li class="nav-item">
-  <a class="nav-link" href="charts.html">
-    <i class="fas fa-fw fa-chart-area"></i>
-    <span>Charts</span></a>
-</li>
-
-<!-- Nav Item - Tables -->
-<li class="nav-item">
-  <a class="nav-link" href="tables.html">
-    <i class="fas fa-fw fa-table"></i>
-    <span>Tables</span></a>
 </li>
